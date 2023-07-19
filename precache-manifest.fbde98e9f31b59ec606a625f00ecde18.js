6db8eba839c27b1175e3be2b153c2e0874f679bf
Shubhdeepgupta <shubhdeepgupta06@gmail.com>
2023-07-19 16:48:34 +0530

    Updates

---
+++ precache-manifest.fbde98e9f31b59ec606a625f00ecde18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5edb7fc20489eed4cf37",
+    "url": "/react-js/static/css/main.f8f102f6.chunk.css"
+  },
+  {
+    "revision": "5edb7fc20489eed4cf37",
+    "url": "/react-js/static/js/main.5edb7fc2.chunk.js"
+  },
+  {
+    "revision": "26c7c62c6189a4a6b534",
+    "url": "/react-js/static/js/1.26c7c62c.chunk.js"
+  },
+  {
+    "revision": "8054e9523315067f1443",
+    "url": "/react-js/static/js/2.8054e952.chunk.js"
+  },
+  {
+    "revision": "954b215a6fa05125048b",
+    "url": "/react-js/static/js/runtime~main.954b215a.js"
+  },
+  {
+    "revision": "aa173fd626166d7b14ced38cffc80f15",
+    "url": "/react-js/index.html"
+  }
+];
